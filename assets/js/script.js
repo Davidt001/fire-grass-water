c2d617c4282df9fe7d747bc43c657c851053ead0
Davidt001 <davidtully01@gmail.com>
2022-07-04 20:38:07 +0000

    add event listenr and callback in java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("connected")
+const choices = document.querySelectorAll(".choices");
+let pScore = 0;
+let cScore = 0;
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", function () {
+    const player = this.value;
+
+    const computerchoice = ["Fire", "Grass", "Water"];
+    const computer = computerchoice[Math.floor(Math.random() * 3)];
